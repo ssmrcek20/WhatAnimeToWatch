cf555ba57422c9179659fc3795b00afe7b12b426
ssmrcek20 <100624076+ssmrcek20@users.noreply.github.com>
2023-08-02 11:58:26 +0200

    Docker set-up

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.Extensions.FileProviders;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -14,6 +16,15 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
+    string parentDirectory = Directory.GetParent(builder.Environment.ContentRootPath).FullName;
+    string externalFolder = Path.Combine(parentDirectory, "Frontend\\dist\\frontend");
+    app.UseFileServer(new FileServerOptions
+    {
+        FileProvider = new PhysicalFileProvider(externalFolder),
+        RequestPath = "",
+        EnableDefaultFiles = true
+    });
 }
 
 app.UseHttpsRedirection();
