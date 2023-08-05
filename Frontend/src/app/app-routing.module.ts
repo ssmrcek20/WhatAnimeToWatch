1f9b16ef9feeb7eddbffd9e758369ffb01da1d0d
ssmrcek20 <100624076+ssmrcek20@users.noreply.github.com>
2023-08-05 15:28:59 +0200

    testing primeng

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -6,11 +6,8 @@ import { QuizComponent } from './quiz/quiz.component';
 
 const routes: Routes = [
   { path: '', component: MainComponent},
-  { path: 'api/Animes', pathMatch: 'full', children: [] },
-  { path: 'api/Animes/:id', pathMatch: 'full', children: [] },
-  { path: 'api/Studios', pathMatch: 'full', children: [] },
-  { path: 'api/Studios/:id', pathMatch: 'full', children: [] },
   { path: 'quiz', component: QuizComponent},
+  
   { path: '404', component: NotFoundComponent },
   { path: '**', redirectTo: '/404' }  
 ];
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -5,9 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
+import { ButtonModule } from 'primeng/button';
+
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
 import { SvgMainComponent } from './svg-main/svg-main.component';
@@ -21,15 +20,13 @@ import { QuizComponent } from './quiz/quiz.component';
     MainComponent,
     SvgMainComponent,
     NotFoundComponent,
-    QuizComponent
+    QuizComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatButtonModule
+    ButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
