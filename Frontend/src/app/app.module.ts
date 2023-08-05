f5d571ce49dabefb9e38a8b548cfe4efa0dd2f9a
ssmrcek20 <100624076+ssmrcek20@users.noreply.github.com>
2023-08-05 18:59:33 +0200

    setting dark and light mode

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
 
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
@@ -27,6 +28,7 @@ import { QuizComponent } from './quiz/quiz.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     ButtonModule,
+    RippleModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Frontend/src/app/header/header.component.ts
+++ Frontend/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2  } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  constructor(private renderer: Renderer2) { }
+  private themeLink = this.renderer.selectRootElement('#theme', true);
+  isLightTheme = true;
 
+  toggleTheme(button: HTMLButtonElement): void {
+    this.isLightTheme = !this.isLightTheme;
+    if (this.isLightTheme) {
+      this.themeLink.setAttribute('href', 'assets/theme.css');     
+      button.classList.remove('pi-sun');
+      button.classList.add('pi-moon');
+    } else {
+      this.themeLink.setAttribute('href', 'assets/theme-dark.css');
+      button.classList.remove('pi-moon');
+      button.classList.add('pi-sun');
+    }
+  }
 }
